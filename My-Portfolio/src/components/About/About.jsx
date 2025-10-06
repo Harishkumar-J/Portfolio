13774e4304e96f0dc40b0cb37dd954dc6e91e948
Harishkumar-J <harishwells@gmail.com>
2025-10-06 16:38:37 +0100

    Updating the About section

--- My-Portfolio/src/components/About/About.jsx
+++ My-Portfolio/src/components/About/About.jsx
@@ -1,6 +1,7 @@
-import aboutImage from '../../../assets/about/aboutImage.png'
-import cursorIcon from '../../../assets/about/cursorIcon.png'
-import serverIcon from '../../../assets/about/serverIcon.png'
+import aboutImage from "../../../assets/about/aboutImage.png";
+import react from "../../../assets/about/react.png";
+import dataAnalyst from "../../../assets/about/dataAnalyst.png";
+import bi from "../../../assets/about/bi.png";
 import styles from "./About.module.css";
 
 export const About = () => {
@@ -8,39 +9,34 @@ export const About = () => {
     <section className={styles.container} id="about">
       <h2 className={styles.title}>About</h2>
       <div className={styles.content}>
-        <img
-          src={aboutImage}
-          alt="Work life"
-          className={styles.aboutImage}
-        />
+        <img src={aboutImage} alt="Work life" className={styles.aboutImage} />
         <ul className={styles.aboutItems}>
           <li className={styles.aboutItem}>
-            <img src={cursorIcon} alt="Cursor icon" />
+            <img src={dataAnalyst} alt="dataAnalysis icon" />
             <div className={styles.aboutItemText}>
-              <h3>Frontend Developer</h3>
+              <h3>Data Analyst</h3>
               <p>
-                I'm a frontend developer with experience in building responsive
-                and optimized sites
+                I love exploring data and uncovering insights that help drive
+                smarter business decisions.
               </p>
             </div>
           </li>
           <li className={styles.aboutItem}>
-            <img src={serverIcon} alt="Server icon" />
+            <img src={bi} alt="bi icon" />
             <div className={styles.aboutItemText}>
-              <h3>Backend Developer</h3>
+              <h3>Business Intelligence Developer</h3>
               <p>
-                I have experience developing fast and optimised back-end systems
-                and APIs
+                I design clean, interactive dashboards and automate reports to
+                make analytics accessible to everyone.
               </p>
             </div>
           </li>
           <li className={styles.aboutItem}>
-            <img src={cursorIcon} alt="UI icon" />
+            <img src={react} alt="frontend icon" />
             <div className={styles.aboutItemText}>
-              <h3>UI Designer</h3>
+              <h3>React Developer</h3>
               <p>
-                I have designed multiple landing pages and have created design
-                systems as well
+                I build responsive, accessible interfaces and connect them to reliable data APIs
               </p>
             </div>
           </li>
