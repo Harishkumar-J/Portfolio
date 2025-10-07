de2bee4c0fb71bdeae21bf1c1e694afdc003fedb
Harishkumar-J <harishwells@gmail.com>
2025-10-07 04:52:26 +0100

    code fix

--- My-Portfolio/src/App.jsx
+++ My-Portfolio/src/App.jsx
@@ -1,7 +1,7 @@
 import styles from "./App.module.css";
 import { Hero } from "./components/Hero/Hero";
 import { About } from "./components/About/About";
-import Navbar from "./components/navBar/navBar";
+import {Navbar} from "./components/navBar/navBar";
 import { Experience } from "./components/Experience/Experience";
 import { Projects } from "./components/Projects/Projects";
 import { Contact } from "./components/Contact/Contact";
